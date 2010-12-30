9e5db7e87b75abcb4d2201c98c77637a0c8bb9e0
daniel <daniel@daniel-virtual-machine.(none)>
2010-12-30 01:57:26 -0800

    before tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ActiveRecord::Base
     validates :name,  :presence => true,
-                    :length => {:minimum => 1, :maximum => 50}
+                    :length => {:minimum => 1, :maximum => 100}
     validates :context, :presence => true,
-                    :length => { :minimum => 1, :maximum => 65536}
+                    :length => { :minimum => 1, :maximum => 1000000}
     validates :title, :presence => true,
-                  :length => { :minimum => 1 , :maximum => 50}
+                  :length => { :minimum => 1 , :maximum => 100}
     self.per_page = 10;
     
+    #search functionality
     def self.search(search, page)
       paginate :per_page => 10, :page => page,
            :conditions => ['name like ? OR context like?', "%#{search}%", "%#{search}%"], :order => '6 DESC'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101221021557) do
+ActiveRecord::Schema.define(:version => 20101229133847) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -20,10 +20,32 @@ ActiveRecord::Schema.define(:version => 20101221021557) do
     t.datetime "updated_at"
   end
 
+  create_table "pos_tests", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.integer  "tag_id"
+    t.string   "tag_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags_posts", :force => true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
